90ceaa674af76b34965138f0dfbe2a5195f581b5
Piter <1181720735@qq.com>
2020-08-31 17:54:27 +0800

    test

--- thread/src/多线程/lock/LockDemo1.java
+++ thread/src/多线程/lock/LockDemo1.java
@@ -3,20 +3,44 @@ package 多线程.lock;
 import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class LockDemo1 {
 
     //总共票数
     private static int i = 100;
 
+    //可重入锁
+    private static Lock lock = new ReentrantLock();
+    //公平重入锁,默认为非公平
+//    private static Lock lock = new ReentrantLock(true);
+
+    //读写锁:用于读多写少的情况
+    private static ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    //读锁
+    private static Lock readLock = readWriteLock.readLock();
+    //写锁
+    private static Lock writeLock = readWriteLock.writeLock();
+
     public static void main(String[] args) throws InterruptedException {
 
         for (int j = 0; j < 10; j++) {
             Thread thread = new Thread(() -> {
+
                 for (int k = 0; k < 100; k++) {
                     if (i >= 1) {
                         System.out.println("获取到第" + (100 - i + 1) + "号票");
-                        i--;
+                        lock.lock();
+                        try {
+                            i--;
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }finally {
+                            lock.unlock();
+                        }
                     }
                 }
             });
