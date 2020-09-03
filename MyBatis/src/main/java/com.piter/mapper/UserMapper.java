d069aa8fb47904b9aed5b1621e1d1effba297da6
Piter <1181720735@qq.com>
2020-09-03 08:39:23 +0800

    mybatis

--- MyBatis/src/main/java/com.piter/mapper/UserMapper.java
+++ MyBatis/src/main/java/com.piter/mapper/UserMapper.java
@@ -2,7 +2,8 @@ package com.piter.mapper;
 
 import com.piter.entity.User;
 import com.piter.pojo.UserDto;
-import org.apache.ibatis.annotations.Param;
+import com.piter.pojo.UserVo;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 import java.util.Map;
@@ -18,4 +19,18 @@ public interface UserMapper {
     User findById(@Param("userName") String name, @Param("age") Integer age);
 
     List<User> findByObject(UserDto dto);
+
+    @Results(id = "UserVo",value = {
+            @Result(property = "id",column = "id",id = true),
+            @Result(property = "userName",column = "user_name"),
+            @Result(property = "age",column = "user_age"),
+            @Result(property = "address",column = "user_address")
+    })
+    @Select(" select id,user_name,user_age,user_address from user WHERE id = #{id,jdbcType=INTEGER}")
+    List<UserVo> findById2(Integer id);
+
+    @ResultMap("UserVo")
+    @Select(" select id,user_name,user_age,user_address from user WHERE user_name = #{user_name,jdbcType=VARCHAR}")
+    UserVo findById3(@Param("userName") String name, @Param("age") Integer age);
+
 }
