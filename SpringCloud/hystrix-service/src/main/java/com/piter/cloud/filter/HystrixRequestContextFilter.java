b8d07521ae515effaf5cc40370f19879fb074096
Piter <1181720735@qq.com>
2021-02-22 17:55:40 +0800

    spring cloud

---
+++ SpringCloud/hystrix-service/src/main/java/com/piter/cloud/filter/HystrixRequestContextFilter.java
@@ -0,0 +1,29 @@
+package com.piter.cloud.filter;
+
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Created by macro on 2019/9/4.
+ */
+@Component
+@WebFilter(urlPatterns = "/*",asyncSupported = true)
+public class HystrixRequestContextFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HystrixRequestContext context = HystrixRequestContext.initializeContext();
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            context.close();
+        }
+    }
+}
